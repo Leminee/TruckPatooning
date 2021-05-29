32a0470c405d64f94acaf7eba2cb2c620a3a80e0
Lemine <lemine.el.agheb1994@gmail.com>
2021-05-29 04:46:23 +0200

    added framework

---
+++ src/main/java/project/truckplatooning/Truck.java
@@ -0,0 +1,95 @@
+package project.truckplatooning;
+
+public class Truck {
+    private Long truckId;
+    private Truck leadTruck;
+    private Integer speed = 0;
+    private Truck truckInFront;
+    private Truck truckBehind;
+
+    public Truck() {
+    }
+
+    public void accelerate() {
+        int accelerationValue = 5;
+        int maxSpeed = 80;
+
+        if (speed >= maxSpeed) {
+            return;
+        }
+        speed += accelerationValue;
+    }
+
+    public  void brake() {
+        int brakeValue = 5;
+
+        if (speed <= 0) {
+            return;
+        }
+
+        speed -= brakeValue;
+    }
+
+    public void stop() {
+        speed = 0;
+    }
+
+    public void joinPlatoon(Truck truck) {
+
+        Truck currentTruck = this;
+
+        while (currentTruck.getTruckBehind() != null) {
+            currentTruck = currentTruck.getTruckBehind();
+
+        }
+        currentTruck.setTruckBehind(truck);
+        truck.setTruckInFront(currentTruck);
+    }
+
+
+    public void leavePlatoon(Truck truck) {
+
+
+    }
+
+    public long getTruckId() {
+        return truckId;
+    }
+
+    public Truck getLeadTruck() {
+        return leadTruck;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Truck getTruckInFront() {
+        return truckInFront;
+    }
+
+    public Truck getTruckBehind() {
+        return truckBehind;
+    }
+    public void setTruckId(long truckId) {
+        this.truckId = truckId;
+    }
+
+    public void setLeadTruck(Truck leadTruck) {
+        this.leadTruck = leadTruck;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setTruckInFront(Truck truckInFront) {
+        this.truckInFront = truckInFront;
+    }
+
+    public void setTruckBehind(Truck truckBehind) {
+        this.truckBehind = truckBehind;
+    }
+
+
+}
---
+++ src/main/java/project/truckplatooning/TruckPlatooningApplication.java
@@ -0,0 +1,13 @@
+package project.truckplatooning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckPlatooningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckPlatooningApplication.class, args);
+	}
+
+}
