66fb2b4ed8152c850c06af70a6f248a7561e6d80
Lemine <lemine.el.agheb1994@gmail.com>
2021-06-23 03:39:08 +0200

    updated monitor

--- src/main/java/de/truckplatooning/Monitoring.java
+++ src/main/java/de/truckplatooning/Monitoring.java
@@ -12,15 +12,15 @@ import java.io.IOException;
 public class Monitoring {
 
     private final RestTemplate restTemplate;
-    private final String BASE_URL = "http://REGISTRATIONSERVICE/api/registration/";
 
     public Monitoring(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @RequestMapping("/monitor")
-    public String getStatus(Model model) throws IOException {
+    public String getStatus(Model model) {
 
+        String BASE_URL = "http://REGISTRATIONSERVICE/api/registration/";
         model.addAttribute("truck", restTemplate.getForObject(BASE_URL + "trucks", Truck[].class));
 
         return "monitor";
--- src/main/java/de/truckplatooning/MonitoringServiceApplication.java
+++ src/main/java/de/truckplatooning/MonitoringServiceApplication.java
@@ -1,9 +1,7 @@
 package de.truckplatooning;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
